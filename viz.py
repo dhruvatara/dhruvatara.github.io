71162e03b5d180dd0e2453699310d7cd3f998e85
dhruvatara <dhruvatara.b@gmail.com>
2020-08-25 19:23:12 -0700

    made updates

--- viz.py
+++ viz.py
@@ -7,7 +7,7 @@ chart_studio.tools.set_credentials_file(username = username,api_key = api_key)
 import chart_studio.plotly as py
 import chart_studio.tools as tls
 
-df = pd.DataFrame({'Skills':['Python','Java','C','C++','R','SQL','MongoDB','Realm'],'Proficiency on scale of 1-5':[3,4,3,4,4,2,3,1]})
+df = pd.DataFrame({'Skills':['Python','Java','C','C++','R','JavaScript','HTML'],'Proficiency on scale of 1-5':[5,5,4,3,4,3,3]})
 #print(df)
 fig = px.bar(df,x='Proficiency on scale of 1-5',y='Skills',orientation='h',width=600,height=600)
 py.plot(fig,filename="skill_prof",auto_open=True)
